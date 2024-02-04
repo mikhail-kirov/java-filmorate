d1ea481d1701526ec19b2f5daabc5073c9b154c5
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-02-04 12:23:45 +0300

    sprint9_v1_1

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 import lombok.AllArgsConstructor;
-import lombok.NonNull;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
