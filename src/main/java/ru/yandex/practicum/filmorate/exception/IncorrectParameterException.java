e53f7d0e27b7c21fecef02a08b58b7f89c6cb436
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-02-23 01:55:56 +0300

    sprint10_v1.1

--- src/main/java/ru/yandex/practicum/filmorate/exception/IncorrectParameterException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/IncorrectParameterException.java
@@ -3,8 +3,9 @@ package ru.yandex.practicum.filmorate.exception;
 import lombok.Getter;
 
 @Getter
-public class IncorrectParameterException extends RuntimeException{
+public class IncorrectParameterException extends RuntimeException {
     private final String parameter;
+
     public IncorrectParameterException(String parameter) {
         this.parameter = parameter;
     }
